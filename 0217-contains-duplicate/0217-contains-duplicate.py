0ac2aafc5ed3cf0d392c2b8ef8e6b14440b4ecd5
Jay Pradeep Ukarde <jayukarde10@gmail.com>
2026-07-18 20:49:50 +0530

    Time: 12 ms (88.8%), Space: 25.9 MB (61.85%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,11 +5,6 @@ class Solution(object):
         :rtype: bool
         """
         return len(nums) != len(set(nums))
-        # hm=set()
-        # for i in nums:
-        #     if i in hm:
-        #         return True
-        #     hm.add(i)                          
-        # return False
+
         
         
